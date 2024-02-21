2e179d493fa29e85d5d3d287fd5db2fc655cf2b6
goki11reds327 <156587241+goki11reds327@users.noreply.github.com>
2024-02-21 23:55:13 +0800

    tobuylist
    買い物リストのページに1日のボタンを押したら箱が一つ、3日のボタンを押したら三つのみ表示されるように作成しました

--- resources/views/tobuylist.blade.php
+++ resources/views/tobuylist.blade.php
@@ -11,15 +11,72 @@
     <header>
         <div class="self_image"><img src="" alt=""></div>
         <div class="display_btn">
-            <a href="" class="btn btn-gradient"><span>１日</span></a>
-            <a href="" class="btn btn-gradient"><span>３日</span></a>
-            <a href="" class="btn btn-gradient"><span>１週間</span></a>
+            <a href="#" class="btn btn-gradient" onclick="showDiv(1)"><span>１日</span></a>
+            <a href="#" class="btn btn-gradient" onclick="showDiv(3)"><span>３日</span></a>
+            <a href="#" class="btn btn-gradient" onclick="showDiv(7)"><span>１週間</span></a>
         </div>
     </header>
 
     <main>
+        <div id="app" class="container">
+            <div class="item monday">
+                <p>月曜日の買い物
+                    <img src="{{ asset('pexels-clem-onojeghuo-175753.jpg') }}" alt="画像">
+                </p>
+            </div>
         
+            <div class="item tuesday">
+                <p>火曜日の買い物
+                    <img src="{{ asset('pexels-clem-onojeghuo-175753.jpg') }}" alt="画像">
+                </p>
+            </div>
+        
+            <div class="item wednesday">
+                <p>水曜日の買い物
+                    <img src="{{ asset('pexels-clem-onojeghuo-175753.jpg') }}" alt="画像">
+                </p>
+            </div>
+
+            <div class="item thursday">
+                <p>木曜日の買い物
+                    <img src="{{ asset('pexels-clem-onojeghuo-175753.jpg') }}" alt="画像">
+                </p>
+            </div>
+
+            <div class="item friday">
+                <p>金曜日の買い物
+                    <img src="{{ asset('pexels-clem-onojeghuo-175753.jpg') }}" alt="画像">
+                </p>
+            </div>
+
+            <div class="item saturday">
+                <p>土曜日の買い物
+                    <img src="{{ asset('pexels-clem-onojeghuo-175753.jpg') }}" alt="画像">
+                </p>
+            </div>
+
+            <div class="item sunday">
+                <p>日曜日の買い物
+                    <img src="{{ asset('pexels-clem-onojeghuo-175753.jpg') }}" alt="画像">
+                </p>
+            </div>
+
+        </div>
     </main>
+
+    <script>
+        function showDiv(days) {
+            var allItems = document.querySelectorAll('.item');
+            allItems.forEach(function(item, index) {
+                if (index < days) {
+                    item.classList.remove('hidden');
+                } else {
+                    item.classList.add('hidden');
+                }
+            });
+        }
+    </script>
+
     
 </body>
 </html>
