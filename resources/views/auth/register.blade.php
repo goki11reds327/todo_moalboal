b96f9a3a3e74a8a31c1b36fee54407189117106f
maka8931 <anpanananan@gmail.com>
2024-02-23 00:01:28 +0800

    main
    修正

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -40,6 +40,20 @@
                         </div>
 
                         <div class="row mb-3">
+                            <label for="avatar" class="col-md-4 col-form-label text-md-end">{{ __('プロフィール画像（サイズは1024kbyteまで') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="avatar" type="file" class="@error('avatar') is-invalid @enderror" name="avatar">
+
+                                @error('avatar')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
