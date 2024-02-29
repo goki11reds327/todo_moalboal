e5d5b0e62c28d412000b8bbd0322807790c0794c
tuchiya0202 <158553615+tuchiya0202@users.noreply.github.com>
2024-03-01 00:52:59 +0800

    change
    change

--- resources/views/buy/index.blade.php
+++ resources/views/buy/index.blade.php
@@ -17,7 +17,6 @@
     <!-- Styles -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" ></script>
-    
     {{-- Style --}}
     <link rel="stylesheet" href="{{ asset('/css/reset.css') }}">
     <link rel="stylesheet" href="{{ asset('/css/buy.css') }}">
